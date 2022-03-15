35dc5498dd7480f33938c11d63d176fb69b0155e
f-deniyi <holarfemilekan049@gmail.com>
2022-03-15 02:35:09 +0100

    update

--- src/views/App/Contact.js
+++ src/views/App/Contact.js
@@ -30,7 +30,7 @@ const Contact = () => {
                                         transition
                                         ease-in-out
                                         m-0
-                                        focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" id="exampleInput7"
+                                        focus:text-gray-700 focus:bg-white focus:border-white active:border-[none] focus:outline-none" id="exampleInput7"
                                         placeholder="Name" />
                                 </div>
                                 <div className="form-group mb-6">
@@ -47,7 +47,7 @@ const Contact = () => {
                                     transition
                                     ease-in-out
                                     m-0
-                                    focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" id="exampleInput8"
+                                    focus:text-gray-700 focus:bg-white focus:border-white active:border-[none] focus:outline-none" id="exampleInput8"
                                         placeholder="Email address" />
                                 </div>
                                 <div className="form-group mb-6">
@@ -66,7 +66,7 @@ const Contact = () => {
             transition
             ease-in-out
             m-0
-            focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none
+            focus:text-gray-700 focus:bg-white focus:border-white active:border-[none] focus:outline-none
           " id="exampleFormControlTextarea13" rows="3" placeholder="Message"></textarea>
                                 </div>
 
@@ -96,18 +96,13 @@ const Contact = () => {
                                     <div className="flex items-start">
                                         <div className="shrink-0">
                                             <div className="p-4 bg-blue-600 rounded-md shadow-md w-14 h-14 flex items-center justify-center">
-                                                <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="headset" className="w-5 text-white"
-                                                    role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-                                                    <path fill="currentColor"
-                                                        d="M192 208c0-17.67-14.33-32-32-32h-16c-35.35 0-64 28.65-64 64v48c0 35.35 28.65 64 64 64h16c17.67 0 32-14.33 32-32V208zm176 144c35.35 0 64-28.65 64-64v-48c0-35.35-28.65-64-64-64h-16c-17.67 0-32 14.33-32 32v112c0 17.67 14.33 32 32 32h16zM256 0C113.18 0 4.58 118.83 0 256v16c0 8.84 7.16 16 16 16h16c8.84 0 16-7.16 16-16v-16c0-114.69 93.31-208 208-208s208 93.31 208 208h-.12c.08 2.43.12 165.72.12 165.72 0 23.35-18.93 42.28-42.28 42.28H320c0-26.51-21.49-48-48-48h-32c-26.51 0-48 21.49-48 48s21.49 48 48 48h181.72c49.86 0 90.28-40.42 90.28-90.28V256C507.42 118.83 398.82 0 256 0z">
-                                                    </path>
-                                                </svg>
+                                                <svg class="w-8 h-8 text-white mx-auto" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path fill="currentColor" d="M172.268 501.67C26.97 291.031 0 269.413 0 192 0 85.961 85.961 0 192 0s192 85.961 192 192c0 77.413-26.97 99.031-172.268 309.67-9.535 13.774-29.93 13.773-39.464 0zM192 272c44.183 0 80-35.817 80-80s-35.817-80-80-80-80 35.817-80 80 35.817 80 80 80z"></path></svg>
+
                                             </div>
                                         </div>
                                         <div className="grow ml-6">
-                                            <p className="font-bold mb-1">Technical support</p>
-                                            <p className="text-gray-500">support@example.com</p>
-                                            <p className="text-gray-500">+1 234-567-89</p>
+                                            <p className="font-bold mb-1">Location</p>
+                                            <p className="text-gray-500">Anywhere</p>
                                         </div>
                                     </div>
                                 </div>
@@ -115,18 +110,12 @@ const Contact = () => {
                                     <div className="flex items-start">
                                         <div className="shrink-0">
                                             <div className="p-4 bg-blue-600 rounded-md shadow-md w-14 h-14 flex items-center justify-center">
-                                                <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="dollar-sign"
-                                                    className="w-3 text-white" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 288 512">
-                                                    <path fill="currentColor"
-                                                        d="M209.2 233.4l-108-31.6C88.7 198.2 80 186.5 80 173.5c0-16.3 13.2-29.5 29.5-29.5h66.3c12.2 0 24.2 3.7 34.2 10.5 6.1 4.1 14.3 3.1 19.5-2l34.8-34c7.1-6.9 6.1-18.4-1.8-24.5C238 74.8 207.4 64.1 176 64V16c0-8.8-7.2-16-16-16h-32c-8.8 0-16 7.2-16 16v48h-2.5C45.8 64-5.4 118.7.5 183.6c4.2 46.1 39.4 83.6 83.8 96.6l102.5 30c12.5 3.7 21.2 15.3 21.2 28.3 0 16.3-13.2 29.5-29.5 29.5h-66.3C100 368 88 364.3 78 357.5c-6.1-4.1-14.3-3.1-19.5 2l-34.8 34c-7.1 6.9-6.1 18.4 1.8 24.5 24.5 19.2 55.1 29.9 86.5 30v48c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16v-48.2c46.6-.9 90.3-28.6 105.7-72.7 21.5-61.6-14.6-124.8-72.5-141.7z">
-                                                    </path>
-                                                </svg>
+                                                <svg class="w-8 h-8 text-white mx-auto" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M493.4 24.6l-104-24c-11.3-2.6-22.9 3.3-27.5 13.9l-48 112c-4.2 9.8-1.4 21.3 6.9 28l60.6 49.6c-36 76.7-98.9 140.5-177.2 177.2l-49.6-60.6c-6.8-8.3-18.2-11.1-28-6.9l-112 48C3.9 366.5-2 378.1.6 389.4l24 104C27.1 504.2 36.7 512 48 512c256.1 0 464-207.5 464-464 0-11.2-7.7-20.9-18.6-23.4z"></path></svg>
                                             </div>
                                         </div>
                                         <div className="grow ml-6">
-                                            <p className="font-bold mb-1">Sales questions</p>
-                                            <p className="text-gray-500">sales@example.com</p>
-                                            <p className="text-gray-500">+1 234-567-89</p>
+                                            <p className="font-bold mb-1">Phone</p>
+                                            <p className="text-gray-500">+2349130771719</p>
                                         </div>
                                     </div>
                                 </div>
@@ -134,8 +123,8 @@ const Contact = () => {
                                     <div className="flex align-start">
                                         <div className="shrink-0">
                                             <div className="p-4 bg-blue-600 rounded-md shadow-md w-14 h-14 flex items-center justify-center">
-                                                <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="newspaper"
-                                                    className="w-5 text-white" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
+                                                <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="email"
+                                                    className="w-5 text-white text-2xl" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
                                                     <path fill="currentColor"
                                                         d="M552 64H88c-13.255 0-24 10.745-24 24v8H24c-13.255 0-24 10.745-24 24v272c0 30.928 25.072 56 56 56h472c26.51 0 48-21.49 48-48V88c0-13.255-10.745-24-24-24zM56 400a8 8 0 0 1-8-8V144h16v248a8 8 0 0 1-8 8zm236-16H140c-6.627 0-12-5.373-12-12v-8c0-6.627 5.373-12 12-12h152c6.627 0 12 5.373 12 12v8c0 6.627-5.373 12-12 12zm208 0H348c-6.627 0-12-5.373-12-12v-8c0-6.627 5.373-12 12-12h152c6.627 0 12 5.373 12 12v8c0 6.627-5.373 12-12 12zm-208-96H140c-6.627 0-12-5.373-12-12v-8c0-6.627 5.373-12 12-12h152c6.627 0 12 5.373 12 12v8c0 6.627-5.373 12-12 12zm208 0H348c-6.627 0-12-5.373-12-12v-8c0-6.627 5.373-12 12-12h152c6.627 0 12 5.373 12 12v8c0 6.627-5.373 12-12 12zm0-96H140c-6.627 0-12-5.373-12-12v-40c0-6.627 5.373-12 12-12h360c6.627 0 12 5.373 12 12v40c0 6.627-5.373 12-12 12z">
                                                     </path>
@@ -143,9 +132,8 @@ const Contact = () => {
                                             </div>
                                         </div>
                                         <div className="grow ml-6">
-                                            <p className="font-bold mb-1">Press</p>
-                                            <p className="text-gray-500">press@example.com</p>
-                                            <p className="text-gray-500">+1 234-567-89</p>
+                                            <p className="font-bold mb-1">Mail</p>
+                                            <p className="text-gray-500">PWH@gmail.org</p>
                                         </div>
                                     </div>
                                 </div>
@@ -162,9 +150,8 @@ const Contact = () => {
                                             </div>
                                         </div>
                                         <div className="grow ml-6">
-                                            <p className="font-bold mb-1">Bug report</p>
-                                            <p className="text-gray-500">bugs@example.com</p>
-                                            <p className="text-gray-500">+1 234-567-89</p>
+                                            <p className="font-bold mb-1">Support</p>
+                                            <p className="text-gray-500">PWHSupport@gmail.com</p>
                                         </div>
                                     </div>
                                 </div>
